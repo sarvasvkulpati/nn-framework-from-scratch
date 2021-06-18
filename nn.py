8e5db7621f730553068696af7fc583418764f5d4
Sarvasv Kulpati <sarvasvkulpati@sarvasvs-air.lan>
2021-06-18 19:17:18 +0800

    removed empty file

--- nn.py
+++ nn.py
@@ -10,10 +10,6 @@ nnfs.init()
 
 X,y = spiral_data(samples=100, classes=3)
 
-# plt.scatter(X[:,0], X[:, 1], c=y, cmap='brg')
-# plt.show()
-
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons, wl1 = 0, wl2 = 0, bl1= 0, bl2 = 0):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
